38e498764060a75f5f3d583131bb7ca2426aedf4
Sword <saif.at.miracle@gmail.com>
2025-04-25 17:04:58 +0300

    Handling Form Submission & Validating User Input

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 const playerConfigOverlayElement = document.getElementById('config-overlay');
-const backdropElement = document.getElementById('backdrop')
+const backdropElement = document.getElementById('backdrop');
+const formChooseNameElement = document.querySelector('form');
+const errorsConfigOutputElement = document.getElementById('config-error');
 
 const editPlayer1ButtonElement = document.getElementById('player-edit-1-button');
 const editPlayer2ButtonElement = document.getElementById('player-edit-2-button');
@@ -9,8 +11,11 @@ const cancelConfigButtonElement = document.getElementById('cancel-config-button'
 const confirmConButtonElement = document.getElementById('confirm-config-button');
 
 
+
 editPlayer1ButtonElement.addEventListener('click', openPlayerConfig);
 editPlayer2ButtonElement.addEventListener('click', openPlayerConfig);
 
 cancelConfigButtonElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
+
+formChooseNameElement.addEventListener('submit',savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -6,4 +6,26 @@ function openPlayerConfig () {
 function closePlayerConfig () {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formChooseNameElement.firstElementChild.classList.remove('error')
+    playerConfigOverlayElement.classList.remove('errors')
+    backdropElement.classList.remove('errors');
+    errorsConfigOutputElement.textContent = '';
+};
+
+function savePlayerConfig (event) {
+    event.preventDefault();
+    const formData = new FormData(event.target); // FormData is a blueprint method we can initiate it by "new" keyword
+    const enteredPlayername = formData.get('playername').trim();// '    Saif Eldin    ' ==>  'Saif Eldin'  & '       ' ==> ''
+
+    if (!enteredPlayername) { // !enteredPlayername = (enteredPlayername === '')  & '' is a Falsy value
+        event.target.firstElementChild.classList.add('error');
+        playerConfigOverlayElement.classList.add('errors')
+        backdropElement.classList.add('errors');
+
+        errorsConfigOutputElement.textContent = 'Please enter a valid name!';
+        errorsConfigOutputElement.style.color = 'var(--errors-color-900)';
+        
+        return;
+    }
+
 };

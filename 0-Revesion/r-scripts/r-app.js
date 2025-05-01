a83ee99d78c7567599c75664cff3b042af1f1dac
Sword <saif.at.miracle@gmail.com>
2025-05-01 23:56:03 +0300

    Adding Game logic

--- 0-Revesion/r-scripts/r-app.js
+++ 0-Revesion/r-scripts/r-app.js
@@ -1,4 +1,13 @@
+const gameData = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
 let editedPlayer = 0;
+let activePlayer = 0;
+let currentRound = 1;
+let gameIsOver = true;
 
 const players = [
     {
@@ -16,14 +25,17 @@ const overlayConfigElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const configErrorsElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
+const gameOverElement = document.getElementById('game-over');
+const winnerNameElement = document.getElementById('winner-name');
 
 const cancelConfigOverlayBtn = document.getElementById('cancel-config-btn');
-
 const editPlayer1Btn = document.getElementById('edit-player-1-btn');
 const editPlayer2Btn = document.getElementById('edit-player-2-btn');
-
-const startNewGameBtn = document.getElementById('');
-
+const startNewGameBtn = document.getElementById('start-game-btn');
+// const gameFieldElement = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.getElementById('game-board');
 
 editPlayer1Btn.addEventListener('click', openConfigOverlay);
 editPlayer2Btn.addEventListener('click', openConfigOverlay);
@@ -32,3 +44,10 @@ cancelConfigOverlayBtn.addEventListener('click', cancelConfigOverlay);
 backdropElement.addEventListener('click', cancelConfigOverlay);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtn.addEventListener('click', startNewGame);
+
+// for (selectedGameField of gameFieldElement) {
+//     selectedGameField.addEventListener('click', selectGameField);
+// };
+gameBoardElement.addEventListener('click', selectGameField);
---
+++ 0-Revesion/r-scripts/r-game.js
@@ -0,0 +1,121 @@
+function resetGameStatus () {
+    activePlayer = 0;
+    currentRound = 1;
+    gameIsOver = false;
+    gameOverElement.firstElementChild.innerHTML = '<h2>You won, <span id="winner-name">PLAYER NAME</span>!</h2>';
+    gameOverElement.style.display = 'none';
+
+    let gameBoardIndex = 0;
+    for (let i=0; i<3; i++) {
+        for (let j=0; j<3; j++) {
+            gameData[i][j] = 0;
+
+            const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+            gameBoardItemElement.textContent ='';
+            gameBoardItemElement.classList.remove('disabled');
+            gameBoardIndex++
+        }
+    }
+}
+
+function startNewGame () {
+    if (players[0].name === '' || players[1].name === '') {
+        alert('Please set custom player names for both players!');
+        return;
+    };
+
+    resetGameStatus();
+
+    activePlayerNameElement.textContent = players[activePlayer].name;
+    gameAreaElement.style.display = 'block';
+};
+
+
+function switchPlayer () {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+
+function selectGameField (event) {
+    if (event.target.tagName !== 'LI') {
+        return;
+    };
+
+    
+    const selectedGameField = event.target;
+    const selectedColumn = selectedGameField.dataset.col - 1;
+    const selectedRow = selectedGameField.dataset.row - 1;
+    if (gameData[selectedRow][selectedColumn]>0) {
+        alert('Please select an empty field')
+        return;
+    }
+    selectedGameField.textContent = players[activePlayer].symbol;
+    selectedGameField.classList.add('disabled');
+
+    gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+    const winnerId = checkForGameOver();
+    
+    currentRound++;
+
+    if ( winnerId !== 0) {
+        endGame(winnerId);
+    }
+
+    
+    switchPlayer();
+
+}
+
+
+function checkForGameOver () {
+    for (let i=0; i < 3; i++ ) {
+        if (
+            gameData[i][0] > 0 
+            && gameData[i][0] ===  gameData[i][1] 
+            && gameData[i][1] ===  gameData[i][2]
+        ) {
+            return gameData[i][0];
+        }
+
+        if (
+            gameData[0][i] > 0 
+            && gameData[0][i] ===  gameData[1][i] 
+            && gameData[1][i] ===  gameData[2][i]
+        ) {
+            return gameData[0][i];
+        }
+        
+    }
+
+    if (gameData[0][0] > 0 && gameData[0][0] ===  gameData[1][1] && gameData[1][1] ===  gameData[2][2]) {
+        return gameData[0][0];
+    }
+
+    if (gameData[0][2] > 0 && gameData[0][2] ===  gameData[1][1] && gameData[1][1] ===  gameData[2][0]) {
+        return gameData[0][2];
+    }
+
+    if (currentRound === 9) {
+        return -1;
+    }
+
+    return 0;
+}
+
+
+function endGame (winnerIndex) {
+    gameIsOver = true;
+    gameOverElement.style.display = 'block';
+    if (winnerIndex > 0) {
+        gameOverElement.firstElementChild.firstElementChild.textContent = players[winnerIndex - 1].name;
+    } else {
+        gameOverElement.firstElementChild.textContent = 'It\'s a Draw';
+    }
+}
